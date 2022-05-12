aee05695a7422474ae5fcd3b53b068b993646b85
YSidorko <yury.sidorko@gmail.com>
2022-05-12 22:39:25 +0200

    - first controller 'messages'

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,18 @@
+import {Controller, Get, Param, Post, Body} from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+
+  @Get()
+  listMessages() {}
+
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id:string) {
+    console.log(id);
+  }
+}
